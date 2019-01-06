4ae73682ec2b254160e442b4e73888ad552f7ef8
Luiz Fernando França <luiz.darthvader@gmail.com>
2019-01-06 13:15:37 -0200

    Initial Commit

---
+++ Navtest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Navtest
+//
+//  Created by Luiz Fernando França on 06/01/19.
+//  Copyright © 2019 Luiz Fernando França. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
